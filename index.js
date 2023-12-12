aa39fcb35dc8e841ab22b473a022b73cc441afc6
Precioussoul <habsoff@gmail.com>
2023-12-12 22:30:27 +0100

    feat: upgrade to bolt.js, slack drop support for nodejs events and webapi longtime ago

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 require("dotenv").config()
 const axios = require("axios")
-const { WebClient } = require("@slack/web-api")
-const { createEventAdapter } = require("@slack/events-api")
-const signingSecret = process.env["SLACK_SIGNING_SECRET"]
-const botToken = process.env["SLACK_BOT_TOKEN"]
+const { App } = require("@slack/bolt")
 
-const slackEvents = createEventAdapter(signingSecret)
-const slackClient = new WebClient(botToken)
-const PORT = process.env.PORT || 4000
+const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+})
+
+/* Add functionality here */
 
 ;
 (async() => {
-    slackEvents.on("app_mention", (event) => {
+    // Start the app
+    await app.start(process.env.PORT || 3000)
+
+    // Listen for an event from the Events API
+    app.event("app_home_opened", ({ event, say }) => {
+        console.log(`Got message from user ${event.user}: ${event.text}`)
+        say(`Hello <@${event.user}>`)
+    })
+    app.event("app_mention", ({ event, say }) => {
         console.log(`Got message from user ${event.user}: ${event.text}`);
         (async() => {
             try {
                 let resp = await axios.get(`https://api.quotable.io/random`)
                 const quote = resp.data.content
                 const author = resp.data.author
-                await slackClient.chat.postMessage({
+                await say({
                     channel: event.channel,
                     text: `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
                 })
@@ -28,27 +36,21 @@ const PORT = process.env.PORT || 4000
         })()
     })
 
-    slackEvents.on("message.app_home", (event) => {
-        console.log(`Got message from user ${event.user}: ${event.text}`);
+    // Convenience method to listen to only `message` events using a string or RegExp
+    app.message(/quote/i, ({ event, say }) => {;
         (async() => {
             try {
                 let resp = await axios.get(`https://api.quotable.io/random`)
                 const quote = resp.data.content
                 const author = resp.data.author
-                await slackClient.chat.postMessage({
-                    channel: event.channel,
-                    channel_type: "app_home",
-                    text: `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
-                })
+                await say(
+                    `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `
+                )
             } catch (error) {
                 console.log(error.data)
             }
         })()
     })
 
-    slackEvents.on("error", console.error)
-
-    slackEvents.start(PORT).then(async() => {
-        console.log(`Slack event started on port ${PORT}`)
-    })
+    console.log("⚡️ Bolt app is running!")
 })()
