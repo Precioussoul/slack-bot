3d5459a01a2b1c7cb8a95b57b15802cd71a31b35
Precioussoul <habsoff@gmail.com>
2023-12-12 18:19:33 +0100

    feat: finalize mention, events, messages"

--- index.js
+++ index.js
@@ -20,7 +20,24 @@ const PORT = process.env.PORT || 4000
                 const author = resp.data.author
                 await slackClient.chat.postMessage({
                     channel: event.channel,
-                    text: `Hello <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
+                    text: `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
+                })
+            } catch (error) {
+                console.log(error.data)
+            }
+        })()
+    })
+
+    slackEvents.on("message.im", (event) => {
+        console.log(`Got message from user ${event.user}: ${event.text}`);
+        (async() => {
+            try {
+                let resp = await axios.get(`https://api.quotable.io/random`)
+                const quote = resp.data.content
+                const author = resp.data.author
+                await slackClient.chat.postMessage({
+                    channel: event.channel,
+                    text: `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
                 })
             } catch (error) {
                 console.log(error.data)
