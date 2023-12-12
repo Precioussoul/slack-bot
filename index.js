8cac085a0d2c5a0d09f7b8df3078658cfdf1b262
Precioussoul <habsoff@gmail.com>
2023-12-12 18:16:31 +0100

    feat: finalize mention, events, messages"

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 require("dotenv").config()
 const axios = require("axios")
-const { App } = require("@slack/bolt")
+const { WebClient } = require("@slack/web-api")
 const { createEventAdapter } = require("@slack/events-api")
-
 const signingSecret = process.env["SLACK_SIGNING_SECRET"]
 const botToken = process.env["SLACK_BOT_TOKEN"]
-const app = new App({
-    signingSecret: signingSecret,
-    token: botToken,
-})
 
 const slackEvents = createEventAdapter(signingSecret)
 const slackClient = new WebClient(botToken)
+const PORT = process.env.PORT || 4000
 
 ;
 (async() => {
-    await app.start(process.env.PORT || 4000)
-
     slackEvents.on("app_mention", (event) => {
         console.log(`Got message from user ${event.user}: ${event.text}`);
         (async() => {
@@ -36,18 +30,7 @@ const slackClient = new WebClient(botToken)
 
     slackEvents.on("error", console.error)
 
-    slackEvents.start(port).then(() => {
-        console.log(`Slack event started on port ${process.env.PORT || 4000}`)
+    slackEvents.start(PORT).then(async() => {
+        console.log(`Slack event started on port ${PORT}`)
     })
-
-    app.message("Quote of the day", async({ message, say }) => {
-        let resp = await axios.get(`https://api.quotable.io/random`)
-        const quote = resp.data.content
-        const author = resp.data.author
-        await say(
-            `Hi, <@${message.user}>, \n Quote of the day:"${quote}" by ${author}`
-        )
-    })
-
-    console.log(`SuperSlack app is running!`)
 })()
