c13f49a899398cac880abe8a99426a255e32550d
Precioussoul <habsoff@gmail.com>
2023-12-13 17:00:09 +0100

    feat: schedule post for 5pm today, tes

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
 require("dotenv").config()
 const axios = require("axios")
-const { App } = require("@slack/bolt")
+const {App} = require("@slack/bolt")
+const schedule = require("node-schedule")
 
 const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
 })
 
+// const rule = new schedule.RecurrenceRule()
+// rule.dayOfWeek = 2
+// rule.hour = 13
+// rule.minute = 42
+// rule.tz = "UTC+01:00"
+
 /* Add functionality here */
+;(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000)
 
-;
-(async() => {
-    // Start the app
-    await app.start(process.env.PORT || 3000)
+  const rule = new schedule.RecurrenceRule()
+  rule.dayOfWeek = 3
+  rule.hour = 17
+  rule.minute = 5
+  rule.tz = "UTC+01:00"
 
-    // Listen for an event from the Events API
-    app.event("app_home_opened", ({ event, say }) => {
-        console.log(`Got message from user ${event.user}: ${event.text}`)
-        say(`Hello <@${event.user}>, How is the day going :slightly_smiling_face:`)
+  const job = schedule.scheduleJob(rule, async () => {
+    console.log("The answer to life, the universe, and everything!")
+    try {
+      let resp = await axios.get(`https://api.quotable.io/random`)
+      const quote = resp.data.content
+      const author = resp.data.author
+
+      app.client.chat.postMessage({
+        channel: "#random",
+        text: `Quote of the day: "${quote}" by ${author} `,
+      })
+    } catch (error) {}
+  })
+
+  job()
+
+  // Listen for an event from the Events API
+  app.event("app_home_opened", ({event, say}) => {
+    console.log(`Got message from user ${event.user}: ${event.text}`)
+    say(`Hello <@${event.user}>, How is the day going :slightly_smiling_face:`)
+  })
+
+  app.event("app_mention", ({event, say}) => {
+    console.log(`Got message from user ${event.user}: ${event.text}`)
+    ;(async () => {
+      try {
+        let resp = await axios.get(`https://api.quotable.io/random`)
+        const quote = resp.data.content
+        const author = resp.data.author
+        await say({
+          channel: event.channel,
+          text: `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
+        })
+      } catch (error) {
+        console.log(error.data)
+      }
+    })()
+  })
+
+  app.client.chat.app // Convenience method to listen to only `message` events using a string or RegExp
+    .message(/quote/i, ({event, say}) => {
+      ;(async () => {
+        try {
+          let resp = await axios.get(`https://api.quotable.io/random`)
+          const quote = resp.data.content
+          const author = resp.data.author
+          await say(
+            `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `
+          )
+        } catch (error) {
+          console.log(error.data)
+        }
+      })()
     })
 
-    app.event("app_mention", ({ event, say }) => {
-        console.log(`Got message from user ${event.user}: ${event.text}`);
-        (async() => {
-            try {
-                let resp = await axios.get(`https://api.quotable.io/random`)
-                const quote = resp.data.content
-                const author = resp.data.author
-                await say({
-                    channel: event.channel,
-                    text: `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `,
-                })
-            } catch (error) {
-                console.log(error.data)
-            }
-        })()
-    })
-
-    // Convenience method to listen to only `message` events using a string or RegExp
-    app.message(/quote/i, ({ event, say }) => {;
-        (async() => {
-            try {
-                let resp = await axios.get(`https://api.quotable.io/random`)
-                const quote = resp.data.content
-                const author = resp.data.author
-                await say(
-                    `Hi <@${event.user}>! :tada: \n  Quote of the day: "${quote}" by ${author} `
-                )
-            } catch (error) {
-                console.log(error.data)
-            }
-        })()
-    })
-
-    console.log("⚡️ Bolt app is running!")
+  console.log("⚡️ Bolt app is running!")
 })()
