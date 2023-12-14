a49bc6bec802596f603f6cfaffa382a2f7a7af87
Precioussoul <habsoff@gmail.com>
2023-12-14 13:40:29 +0100

    feat: tech discussion added

--- index.js
+++ index.js
@@ -8,6 +8,94 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 })
 
+const techDiscussions = [
+  {
+    id: 1,
+    discussionName:
+      "Choosing the Right Tech Stack for Your Web Development Project: Considerations and Best Practices.",
+  },
+  {
+    id: 2,
+    discussionName:
+      "Full Stack vs. Microservices: Pros and Cons in Modern Software Architecture.",
+  },
+  {
+    id: 3,
+    discussionName:
+      "Frontend Frameworks Showdown: React vs. Vue vs. Angular - Which One Suits Your Project?",
+  },
+  {
+    id: 4,
+    discussionName:
+      "The Evolution of Backend Technologies: From Monolithic to Serverless Architectures.",
+  },
+  {
+    id: 5,
+    discussionName:
+      "Database Dilemmas: Comparing SQL and NoSQL in Different Tech Stacks.",
+  },
+  {
+    id: 6,
+    discussionName:
+      "Containerization with Docker and Kubernetes: Streamlining Deployment in Tech Stacks.",
+  },
+  {
+    id: 7,
+    discussionName:
+      "Serverless Computing: How Does It Fit Into Your Tech Stack Strategy?",
+  },
+  {
+    id: 8,
+    discussionName:
+      "The Battle of the Programming Languages: Python, JavaScript, Java, and More in Tech Stacks.",
+  },
+  {
+    id: 9,
+    discussionName:
+      "DevOps in Action: Integrating Continuous Integration/Continuous Deployment (CI/CD) Pipelines in Your Tech Stack.",
+  },
+  {
+    id: 10,
+    discussionName:
+      "Choosing the Right Cloud Service Provider for Your Tech Stack: AWS, Azure, or Google Cloud?",
+  },
+  {
+    id: 11,
+    discussionName:
+      "Micro Frontends: Breaking Down Monolithic Frontend Architectures for Scalability.",
+  },
+  {
+    id: 12,
+    discussionName:
+      "GraphQL vs. REST: Optimizing API Design in Your Tech Stack.",
+  },
+  {
+    id: 13,
+    discussionName:
+      "Mobile App Development: Native vs. Cross-Platform in Your Tech Stack.",
+  },
+  {
+    id: 14,
+    discussionName:
+      "The Role of Progressive Web Apps (PWAs) in Modern Tech Stacks.",
+  },
+  {
+    id: 15,
+    discussionName:
+      "The Importance of Monitoring and Logging in Maintaining a Healthy Tech Stack.",
+  },
+]
+
+function getRandomDiscussion() {
+  const randomIndex = Math.floor(Math.random() * techDiscussions.length)
+  return techDiscussions[randomIndex]
+}
+
+// Example: Displaying a random discussion
+const randomDiscussion = getRandomDiscussion()
+console.log("Random Tech Discussion:")
+console.log(randomDiscussion.discussionName)
+
 // const rule = new schedule.RecurrenceRule()
 // rule.dayOfWeek = 2
 // rule.hour = 13
@@ -20,20 +108,16 @@ const app = new App({
   await app.start(process.env.PORT || 3000)
 
   const rule = new schedule.RecurrenceRule()
-  rule.dayOfWeek = 2
+  rule.dayOfWeek = 4
   rule.hour = 13
-  rule.minute = 1
+  rule.minute = 45
   rule.tz = "UTC+01:00"
 
   const job = schedule.scheduleJob(rule, async () => {
     try {
-      let resp = await axios.get(`https://api.quotable.io/random`)
-      const quote = resp.data.content
-      const author = resp.data.author
-
       app.client.chat.postMessage({
         channel: "#random",
-        text: `Quote of the day: "${quote}" by ${author} `,
+        text: `Hello <@#random>, Its Another TechTuesday :technologist: \n  Let give our input to the discussion below and have a nice engagement. \n \n  *${randomDiscussion}*   `,
       })
     } catch (error) {
       console.log("schedule error: " + error)
